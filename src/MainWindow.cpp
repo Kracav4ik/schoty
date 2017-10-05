7947ee323a23521eb5275f3c3bc9ad001a49b6fb
Kracav4ik <dikama2013@yandex.ru>
2017-10-05 04:23:13 +0300

    Registers and memory will be signed after all

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -16,11 +16,11 @@ void MainWindow::updateUi() {
     for (int address = 0; address < Processor::MEM_SIZE; ++address) {
         int row = address >> 5;
         int col = address & ((1 << 5) - 1);
-        QString text = QString("%1").arg(pro.memory[address], 2, 16, QLatin1Char('0'));
+        QString text = QString("%1").arg(static_cast<uint8_t>(pro.memory[address]), 2, 16, QLatin1Char('0'));
         memoryTable->setItem(row, col, new QTableWidgetItem(text));
     }
     for (int reg = 0; reg < Processor::REG_COUNT; ++reg) {
-        getReg(reg)->setValue(pro.registers[reg]);
+        getReg(reg)->setValue(static_cast<uint8_t>(pro.registers[reg]));
     }
 }
 
--- src/Processor.h
+++ src/Processor.h
@@ -8,8 +8,8 @@ public:
     static const int REG_COUNT = 16;
     static const int MEM_SIZE = 256;
 
-    uint8_t registers[REG_COUNT];
-    uint8_t memory[MEM_SIZE];
+    int8_t registers[REG_COUNT];
+    int8_t memory[MEM_SIZE];
     uint8_t commandOffset = 0;
 
     Processor();
